f3f2feab1fe6ab4eb825b38421f9acbf8b762a57
Raviraja <raviraja.ghanta@gmail.com>
2018-08-18 17:30:10 +0530

    adding todo functionality implemented

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.decorators.http import require_POST
+
+from .models import Todo
+from .forms import TodoForm
 
 
 def index(request):
-    context = {}
+    # main page controller
+    todo_list = Todo.objects.order_by('id')
+    form = TodoForm()
+
+    context = {'todo_list': todo_list, 'form': form}
     return render(request, 'todo/index.html', context)
+
+
+@require_POST
+def addTodo(request):
+    # adds a todo item
+    form = TodoForm(request.POST)
+    if form.is_valid():
+        new_todo = Todo(text=request.POST['text'])
+        new_todo.save()
+
+    return redirect('index')
