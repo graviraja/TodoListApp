6a54e476aed627434c5d9b7e3dfbfba8bacaea38
Raviraja <raviraja.ghanta@gmail.com>
2018-08-18 17:43:11 +0530

    added the functionality of completing task

--- todo/urls.py
+++ todo/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name="index"),
-    path('add', views.addTodo, name="add")
+    path('add', views.addTodo, name="add"),
+    path('complete/<todo_id>', views.completeTodo, name="complete")
 ]
--- todo/views.py
+++ todo/views.py
@@ -23,3 +23,11 @@ def addTodo(request):
         new_todo.save()
 
     return redirect('index')
+
+
+def completeTodo(request, todo_id):
+    todo = Todo.objects.get(pk=todo_id)
+    todo.completed = True
+    todo.save()
+
+    return redirect('index')
